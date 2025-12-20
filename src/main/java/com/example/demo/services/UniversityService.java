94d13facaa9903bf1a9f951dde3e6c1966c0d3bc
bala-amypo <bala@amypo.in>
2025-12-20 14:03:37 +0000

    User commit

--- src/main/java/com/example/demo/services/UniversityService.java
+++ src/main/java/com/example/demo/services/UniversityService.java
@@ -1,3 +1,17 @@
 package com.example.demo.service;
-import java.util.*;
-import com.example.demo.entity.University;
+
+import com.example.demo.model.Garage;
+import java.util.List;
+
+public interface GarageService {
+
+    Garage createGarage(Garage garage);
+    Garage updateGarage(Long id, Garage garage);
+    Garage getGarageById(Long id);
+
+    // Get all garages
+    List<Garage> getAllGarage();
+
+    // Deactivate a garage by ID
+    void deactivateGarage(Long id);
+}
