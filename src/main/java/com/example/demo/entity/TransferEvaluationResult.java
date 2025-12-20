890682f252951ea957502d32d5af209c967f3ed0
bala-amypo <bala@amypo.in>
2025-12-20 18:35:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferEvaluationResult.java
+++ src/main/java/com/example/demo/entity/TransferEvaluationResult.java
@@ -14,21 +14,21 @@ import lambok.NoArgsConstructor;
 @AllArgsConstructor
 public class University{
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="name",unique=true)
-    private Long id;
-    @OneToOne
-    @JoinColumn(name="sourceCourse_id",nullable=false)
-    private Course sourceCourse;
-    @OneToOne
-    @JoinColumn(name="targetCourse_id",nullable=false)
-    private Course targetCourse;
-    private Double overlapPercentage;
-    private Integer creditHourDifference;
-    private Boolean isEligibleForTransfer;
-    private LocalDateTime evaluatedAt;
-    @NotBlank(message="Evaluation reason is required")
-    private String notes;
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
+            @Column(name="name",unique=true)
+                private Long id;
+                    @OneToOne
+                        @JoinColumn(name="sourceCourse_id",nullable=false)
+                            private Course sourceCourse;
+                                @OneToOne
+                                    @JoinColumn(name="targetCourse_id",nullable=false)
+                                        private Course targetCourse;
+                                            private Double overlapPercentage;
+                                                private Integer creditHourDifference;
+                                                    private Boolean isEligibleForTransfer;
+                                                        private LocalDateTime evaluatedAt;
+                                                            @NotBlank(message="Evaluation reason is required")
+                                                                private String notes;
 
 
-}
+                                                                }
