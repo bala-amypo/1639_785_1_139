53222479c3e8b2a0989474ab28d4ea64fe0a448d
bala-amypo <bala@amypo.in>
2025-12-20 18:38:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferEvaluationResult.java
+++ src/main/java/com/example/demo/entity/TransferEvaluationResult.java
@@ -1,34 +1,41 @@
 package com.example.demo.entity;
-import java.util.*;
+import java.time.LocalDateTime;
+// import java.util.*;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lambok.Data;
-import lambok.AllArgsConstructor;
-import lambok.NoArgsConstructor;
+
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class University{
+public class TransferEvaluationResult{
     @Id
-        @GeneratedValue(strategy=GenerationType.IDENTITY)
-            @Column(name="name",unique=true)
-                private Long id;
-                    @OneToOne
-                        @JoinColumn(name="sourceCourse_id",nullable=false)
-                            private Course sourceCourse;
-                                @OneToOne
-                                    @JoinColumn(name="targetCourse_id",nullable=false)
-                                        private Course targetCourse;
-                                            private Double overlapPercentage;
-                                                private Integer creditHourDifference;
-                                                    private Boolean isEligibleForTransfer;
-                                                        private LocalDateTime evaluatedAt;
-                                                            @NotBlank(message="Evaluation reason is required")
-                                                                private String notes;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="name",unique=true)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name="sourceCourse_id",nullable=false)
+    private Course sourceCourse;
+    @OneToOne
+    @JoinColumn(name="targetCourse_id",nullable=false)
+    private Course targetCourse;
+    private Double overlapPercentage;
+    private Integer creditHourDifference;
+    private Boolean isEligibleForTransfer;
+    private LocalDateTime evaluatedAt;
+    @NotBlank(message="Evaluation reason is required")
+    private String notes;
 
 
-                                                                }
+}
