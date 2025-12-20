21ed64d5c784e57387af9cf3edc8239da4114d5d
bala-amypo <bala@amypo.in>
2025-12-20 16:14:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -19,7 +19,7 @@ public class User{
     private Long id;
     private String email;
     private String password;
-    private  roles;
+    private Set<String> roles;
     private LocalDateTime createdAt;
 
 }
