aca95b7252b6e4f1e1d8a044a726fa34fc2a7982
bala-amypo <bala@amypo.in>
2025-12-20 20:17:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.model.;
+import java.util.List;
+
+public interface  UniversityService{
+    University createTopic(Course course);
+    University updateTopic(Long id,CourseContentTopic course);
+    University getTopicById(Long id);
+}
