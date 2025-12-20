5fb94f89c003630e7c86ca487d7fa6abd294ce0f
bala-amypo <bala@amypo.in>
2025-12-20 12:53:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/TransferEvaluationResult.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+import java.util.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedType;
+import jakarta.persistence.GenerationType;
+import lambok.Data;
+import lambok.AllArgsConstructor;
+import lambok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class University{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="name",unique=true)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name="sourceCourse_id",nullable=false)
+    private Course sourceCourse;
+    @OneToOne
+    @JoinColumn(name="targetCourse_id",nullable=false)
+    private Course targetCourse;
+    private Double ;
+    private String description;
+    private String department;
+    private Integer creditHours;
+    private Boolean active =true;
+
+
+}
