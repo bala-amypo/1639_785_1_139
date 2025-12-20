039b9ebfc34582984bfc2e6749432ea0cbc4d350
bala-amypo <bala@amypo.in>
2025-12-20 14:18:38 +0000

    User commit

--- src/main/java/com/example/demo/services/UniversityService.java
+++ src/main/java/com/example/demo/services/UniversityService.java
@@ -4,8 +4,8 @@ import com.example.demo.model.Garage;
 import java.util.List;
 
 public interface  UniversityService{
-    University (University );
-    Garage (Long id, Garage garage);
+    U (University );
+    University (Long id, Garage garage);
     Garage (Long id);
     List<Garage> ();
     void (Long id);
