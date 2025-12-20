71e2b1488e74cb98ff745171930da6f40bde103c
bala-amypo <bala@amypo.in>
2025-12-20 12:47:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+import java.util.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedType;
+import jakarta.persistence.GenerationType;
+import lambok.Data;
+import lambok.AllArgsConstructor;
+import lambok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class University{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="name",unique=true)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name="course_id",nullable=false)
+    private Course course;
+    private String topicName;
+    private Double weightPercentage;
+}
