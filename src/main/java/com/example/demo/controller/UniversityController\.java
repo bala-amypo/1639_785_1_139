dba28029a2d14de4c6eb8ab21d0b879f35709d7c
bala-amypo <bala@amypo.in>
2025-12-20 21:26:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/UniversityController\.java
+++ src/main/java/com/example/demo/controller/UniversityController\.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.model.University;
+import com.example.demo.entity.University;
 import com.example.demo.service.UniversityService;
 
 @RestController
