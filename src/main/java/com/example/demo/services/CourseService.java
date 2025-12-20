86a671607d8b252fe2af5bbc131873bb3a6bb52a
bala-amypo <bala@amypo.in>
2025-12-20 20:02:53 +0000

    User commit

--- src/main/java/com/example/demo/services/CourseService.java
+++ src/main/java/com/example/demo/services/CourseService.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Garage;
+import com.example.demo.model.service;
 import java.util.List;
 
 public interface  CourseService{
--- src/main/java/com/example/demo/services/TransferEvaluationService.java
+++ src/main/java/com/example/demo/services/TransferEvaluationService.java
@@ -1,11 +1,8 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Garage;
+import com.example.demo.model.;
 import java.util.List;
 
 public interface  TransferEvaluationService{
-    TransferEvaluation updateUniversity(Long id,University univ);
-    University getUniversityById(Long id);
-    List<University> getAllUniversities();
-    void deactivateUniversity(Long id);
+    TransferEvaluation getEvaluationById(Long id);
 }
