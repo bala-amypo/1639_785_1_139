db2b317621b3c419e83a6f12545b2e03e2680ae6
bala-amypo <bala@amypo.in>
2025-12-21 13:18:26 +0000

    User commit

--- src/main/java/com/example/demo/service/TransferEvaluationService.java
+++ src/main/java/com/example/demo/service/TransferEvaluationService.java
@@ -1,12 +1,12 @@
 package com.example.demo.service;
 
 import java.util.List;
-import com.example.demo.entity.TransferEvaluation;
+import com.example.demo.entity.TransferEvaluationResult;
 
 public interface TransferEvaluationService{
-    TransferEvaluation postData4(TransferEvaluation use);
-    List<TransferEvaluation>getAllData4();
+    TransferEvaluationResult postData4(TransferEvaluationResult use);
+    List<TransferEvaluationResult>getAllData4();
     String  DeleteData4(Long id);
-    TransferEvaluation getEvaluationById(Long id);         
-    TransferEvaluation updateData4(Long id,TransferEvaluation entity);                                                        
+    TransferEvaluationResult getEvaluationById(Long id);         
+    TransferEvaluationResult updateData4(Long id,TransferEvaluationResult entity);                                                        
 }
