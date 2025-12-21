4d3041c5df8941277997a76dd612ea4e7460ebb3
bala-amypo <bala@amypo.in>
2025-12-21 14:03:26 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -6,4 +6,9 @@ import java.util.List;
 public interface  AuthService{
     User register(User user);
     User login(String email,String password);
+    User getData6(Long id); 
+    List<User>getAllData6();
+    User updateData6(Long id,User use);
+    String  DeleteData6(Long id);
+
 }
