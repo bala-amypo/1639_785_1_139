2e790df119780f3a3712481db273ef117bd8137c
bala-amypo <bala@amypo.in>
2025-12-21 19:14:27 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TransferRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TransferRuleServiceImpl.java
@@ -55,7 +55,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.TransferRule;
 import com.example.demo.repository.TransferRuleRepository;
 import com.example.demo.service.TransferRuleService;
