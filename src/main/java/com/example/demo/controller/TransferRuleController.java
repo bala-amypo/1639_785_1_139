6d24acc94f38c8458fb96f49cc3b803b4934858e
bala-amypo <bala@amypo.in>
2025-12-21 16:51:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/TransferRuleController.java
+++ src/main/java/com/example/demo/controller/TransferRuleController.java
@@ -1,18 +1,18 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import java.util.*;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import com.example.demo.entity.TransferRule;
-// import com.example.demo.service.TransferRuleService;
-// import jakarta.validation.Valid;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.entity.TransferRule;
+import com.example.demo.service.TransferRuleService;
+import jakarta.validation.Valid;
 
 // @RequestMapping("/TransferRule")
 // @RestController
