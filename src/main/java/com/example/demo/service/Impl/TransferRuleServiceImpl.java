648aa3450621bd7b48da8c1059e9ce7f953535dd
bala-amypo <bala@amypo.in>
2025-12-21 17:47:28 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TransferRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TransferRuleServiceImpl.java
@@ -39,7 +39,7 @@ public class TransferRuleServiceImpl implements TransferRuleService {
     // }
     @Override
     public TransferRule getRuleById(Long id) {
-    return TransferRuleRepository.findById(id)
+    return ruleRepository.findById(id)
             .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "TransferRule not found with id " + id)
             );
 }
