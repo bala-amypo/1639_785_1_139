bb0f17d959bfc1cd9933621447b0febe01efb9a8
bala-amypo <bala@amypo.in>
2025-12-21 17:38:27 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TransferEvaluationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TransferEvaluationServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.impl;
 
 import java.util.List;
+import org.springframework.web.server.ResponseStatusException;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;   
 import com.example.demo.entity.TransferEvaluationResult;
@@ -29,12 +31,20 @@ public class TransferEvaluationServiceImpl implements TransferEvaluationService{
     public TransferEvaluationResult getEvaluationById(Long id){
     return used.findById(id).orElse(null);
     }
-    @Override
-    public TransferEvaluationResult updateData4(Long id,TransferEvaluationResult entity){
-        if(used.existsById(id)){
-            entity.setId(id);
-            return used.save(entity);
-        } 
-        return null;
-    }
+    // @Override
+    // public TransferEvaluationResult updateData4(Long id,TransferEvaluationResult entity){
+    //     if(used.existsById(id)){
+    //         entity.setId(id);
+    //         return used.save(entity);
+    //     } 
+    //     return null;
+    // }
+@Override
+public TransferRule getRuleById(Long id) {
+    return transferRuleRepository.findById(id)
+            .orElseThrow(() ->
+                new ResponseStatusException(NOT_FOUND, "TransferRule not found with id " + id)
+            );
+}
+
 }
