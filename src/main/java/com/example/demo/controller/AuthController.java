8b7d740ec160b2d1a5f940fbfad88e278408a805
bala-amypo <bala@amypo.in>
2025-12-21 14:15:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -12,41 +12,35 @@ import com.example.demo.service.AuthService;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    @Autowired
+    @Autowired AuthService authService;
 
-    // Register
     @PostMapping("/register")
     public User register(@RequestBody User user) {
         return authService.register(user);
     }
 
-    // Login
     @PostMapping("/login")
     public User login(@RequestParam String email,
                       @RequestParam String password) {
         return authService.login(email, password);
     }
 
-    // Get by ID
     @GetMapping("/{id}")
     public User getById(@PathVariable Long id) {
         return authService.getData6(id);
     }
 
-    // Get all
     @GetMapping
     public List<User> getAll() {
         return authService.getAllData6();
     }
 
-    // Update
     @PutMapping("/{id}")
     public User update(@PathVariable Long id,
                        @RequestBody User user) {
         return authService.updateData6(id, user);
     }
 
-    // Delete
     @DeleteMapping("/{id}")
     public String delete(@PathVariable Long id) {
         return authService.DeleteData6(id);
