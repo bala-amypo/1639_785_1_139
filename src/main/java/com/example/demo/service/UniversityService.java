5a8e77c5c8130b4bbfe86dbb5f1fe163db659d18
bala-amypo <bala@amypo.in>
2025-12-21 12:03:26 +0000

    User commit

--- src/main/java/com/example/demo/service/UniversityService.java
+++ src/main/java/com/example/demo/service/UniversityService.java
@@ -4,7 +4,8 @@ import java.util.List;
 import com.example.demo.entity.University;
 
 public interface UniversityService{
-    University postData1(University use);
+    University createUniversity(University univ);
+     University updateData1(Long id,University entity);
     List<University>getAllData1();
     String  DeleteData1(Long id);
     University getData1(Long id);         
