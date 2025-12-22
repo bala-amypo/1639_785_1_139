066f4a8c6fa64ecf305657cb05daf14a36b84a19
bala-amypo <bala@amypo.in>
2025-12-22 03:20:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -1,7 +1,7 @@
 package com.example.demo.entity;
 // import java.util.*;
 
-// import jakarta.persistence.Column;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
