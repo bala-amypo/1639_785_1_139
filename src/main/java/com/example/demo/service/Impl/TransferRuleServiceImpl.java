06b81b9c8d152e0234353bf2b5aa05452458ca97
bala-amypo <bala@amypo.in>
2025-12-22 01:42:53 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TransferRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TransferRuleServiceImpl.java
@@ -1,84 +1,84 @@
-// package com.example.demo.service.impl;
-
-// import java.util.List;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import org.springframework.web.server.ResponseStatusException;
-// import static org.springframework.http.HttpStatus.NOT_FOUND;
-// import com.example.demo.entity.TransferRule;
-// import com.example.demo.entity.University;
-// import com.example.demo.repository.TransferRuleRepository;
-// import com.example.demo.repository.UniversityRepository;
-// import com.example.demo.service.TransferRuleService;
-
-// @Service
-// public class TransferRuleServiceImpl implements TransferRuleService {
-
-//     @Autowired
-//     TransferRuleRepository ruleRepository;
-
-//     @Override
-//     public TransferRule createRule(TransferRule rule) {
-//         return ruleRepository.save(rule);
-//     }
-
-//     @Override
-//     public TransferRule updateRule(Long id, TransferRule rule) {
-//         if (ruleRepository.existsById(id)) {
-//             rule.setId(id);
-//             return ruleRepository.save(rule);
-//         }
-//         throw new RuntimeException("not found");
-//     }
-
-   
-//     @Override
-//     public TransferRule getRuleById(Long id) {
-//     return ruleRepository.findById(id)
-//             .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "TransferRule not found with id " + id)
-//             );
-// }
-//     @Override
-//     public String DeleteData5(Long id){
-//         ruleRepository.deleteById(id);
-//         return "Deleted successfully";
-//     }
-//     @Override
-//     public List<TransferRule>getAllData5(){
-//         return ruleRepository.findAll();
-//     }
-// }
 package com.example.demo.service.impl;
 
-import org.springframework.stereotype.Service;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import com.example.demo.entity.TransferRule;
+import com.example.demo.entity.University;
 import com.example.demo.repository.TransferRuleRepository;
+import com.example.demo.repository.UniversityRepository;
 import com.example.demo.service.TransferRuleService;
 
 @Service
 public class TransferRuleServiceImpl implements TransferRuleService {
 
-    private final TransferRuleRepository transferRuleRepository;
-
-    public TransferRuleServiceImpl(TransferRuleRepository transferRuleRepository) {
-        this.transferRuleRepository = transferRuleRepository;
-    }
+    @Autowired
+    TransferRuleRepository ruleRepository;
 
     @Override
     public TransferRule createRule(TransferRule rule) {
-        return transferRuleRepository.save(rule);
+        return ruleRepository.save(rule);
     }
 
+    @Override
+    public TransferRule updateRule(Long id, TransferRule rule) {
+        if (ruleRepository.existsById(id)) {
+            rule.setId(id);
+            return ruleRepository.save(rule);
+        }
+        throw new RuntimeException("not found");
+    }
+
+   
     @Override
     public TransferRule getRuleById(Long id) {
-        return transferRuleRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Rule not found"));
-    }
-
+    return ruleRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "TransferRule not found with id " + id)
+            );
+}
     @Override
-    public List<TransferRule> getAllRules() {
-        return transferRuleRepository.findAll();
+    public String DeleteData5(Long id){
+        ruleRepository.deleteById(id);
+        return "Deleted successfully";
+    }
+    @Override
+    public List<TransferRule>getAllData5(){
+        return ruleRepository.findAll();
     }
 }
+// package com.example.demo.service.impl;
+
+// import org.springframework.stereotype.Service;
+// import java.util.List;
+// import com.example.demo.entity.TransferRule;
+// import com.example.demo.repository.TransferRuleRepository;
+// import com.example.demo.service.TransferRuleService;
+
+// @Service
+// public class TransferRuleServiceImpl implements TransferRuleService {
+
+//     private final TransferRuleRepository transferRuleRepository;
+
+//     public TransferRuleServiceImpl(TransferRuleRepository transferRuleRepository) {
+//         this.transferRuleRepository = transferRuleRepository;
+//     }
+
+//     @Override
+//     public TransferRule createRule(TransferRule rule) {
+//         return transferRuleRepository.save(rule);
+//     }
+
+//     @Override
+//     public TransferRule getRuleById(Long id) {
+//         return transferRuleRepository.findById(id)
+//                 .orElseThrow(() -> new RuntimeException("Rule not found"));
+//     }
+
+//     @Override
+//     public List<TransferRule> getAllRules() {
+//         return transferRuleRepository.findAll();
+//     }
+// }
--- src/main/java/com/example/demo/service/Impl/UniversityServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UniversityServiceImpl.java
@@ -1,91 +1,91 @@
-// package com.example.demo.service.impl;
-
-// import java.util.List;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;   
-// import com.example.demo.entity.University;
-// import com.example.demo.repository.UniversityRepository;
-// // import org.springframework.web.bind.annotation.PathVariable;
-// import com.example.demo.service.UniversityService;                
-
-// @Service
-// public class UniversityServiceImpl implements UniversityService{
-
-//     @Autowired UniversityRepository used;
-//     @Override
-//     public University createUniversity(University univ){
-//         return used.save(univ);  
-//     }
-//     @Override
-//     public List<University>getAllUniversities(){
-//         return used.findAll();
-//     }
-//     @Override
-//     public University getUniversityById(Long id){
-//     return used.findById(id).orElseThrow(()-> new RuntimeException("not found"));
-//     }
-//     @Override
-//     public University updateUniversity(Long id,University univ){
-//         if(used.existsById(id)){
-//             univ.setId(id);
-//             return used.save(univ);
-//         } 
-//         return null;
-//     }
-//     @Override
-//     public String DeleteData1(Long id){
-//         used.deleteById(id);
-//         return "Deleted successfully";
-//     }
-   
-// }
 package com.example.demo.service.impl;
 
-import org.springframework.stereotype.Service;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;   
 import com.example.demo.entity.University;
 import com.example.demo.repository.UniversityRepository;
-import com.example.demo.service.UniversityService;
+// import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.service.UniversityService;                
 
 @Service
-public class UniversityServiceImpl implements UniversityService {
+public class UniversityServiceImpl implements UniversityService{
 
-    private final UniversityRepository universityRepository;
-
-    public UniversityServiceImpl(UniversityRepository universityRepository) {
-        this.universityRepository = universityRepository;
-    }
-
+    @Autowired UniversityRepository used;
     @Override
-    public University createUniversity(University univ) {
-        return universityRepository.save(univ);
+    public University createUniversity(University univ){
+        return used.save(univ);  
     }
-
     @Override
-    public University updateUniversity(Long id, University univ) {
-        University existing = universityRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("University not found"));
-        existing.setName(univ.getName());
-        existing.setLocation(univ.getLocation());
-        return universityRepository.save(existing);
+    public List<University>getAllUniversities(){
+        return used.findAll();
     }
-
     @Override
-    public University getUniversityById(Long id) {
-        return universityRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("University not found"));
+    public University getUniversityById(Long id){
+    return used.findById(id).orElseThrow(()-> new RuntimeException("not found"));
     }
-
     @Override
-    public List<University> getAllUniversities() {
-        return universityRepository.findAll();
+    public University updateUniversity(Long id,University univ){
+        if(used.existsById(id)){
+            univ.setId(id);
+            return used.save(univ);
+        } 
+        return null;
     }
-
     @Override
-    public String deleteData(Long id) {
-        universityRepository.deleteById(id);
-        return "Deleted Successfully";
+    public String DeleteData1(Long id){
+        used.deleteById(id);
+        return "Deleted successfully";
     }
+   
 }
+// package com.example.demo.service.impl;
+
+// import org.springframework.stereotype.Service;
+// import java.util.List;
+// import com.example.demo.entity.University;
+// import com.example.demo.repository.UniversityRepository;
+// import com.example.demo.service.UniversityService;
+
+// @Service
+// public class UniversityServiceImpl implements UniversityService {
+
+//     private final UniversityRepository universityRepository;
+
+//     public UniversityServiceImpl(UniversityRepository universityRepository) {
+//         this.universityRepository = universityRepository;
+//     }
+
+//     @Override
+//     public University createUniversity(University univ) {
+//         return universityRepository.save(univ);
+//     }
+
+//     @Override
+//     public University updateUniversity(Long id, University univ) {
+//         University existing = universityRepository.findById(id)
+//                 .orElseThrow(() -> new RuntimeException("University not found"));
+//         existing.setName(univ.getName());
+//         existing.setLocation(univ.getLocation());
+//         return universityRepository.save(existing);
+//     }
+
+//     @Override
+//     public University getUniversityById(Long id) {
+//         return universityRepository.findById(id)
+//                 .orElseThrow(() -> new RuntimeException("University not found"));
+//     }
+
+//     @Override
+//     public List<University> getAllUniversities() {
+//         return universityRepository.findAll();
+//     }
+
+//     @Override
+//     public String deleteData(Long id) {
+//         universityRepository.deleteById(id);
+//         return "Deleted Successfully";
+//     }
+// }
 
 
