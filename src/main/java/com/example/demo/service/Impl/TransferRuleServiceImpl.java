6c937aea89d4b61ec4b57938088becd422d417cf
bala-amypo <bala@amypo.in>
2025-12-24 16:09:59 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TransferRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TransferRuleServiceImpl.java
@@ -16,17 +16,17 @@ import com.example.demo.service.TransferRuleService;
 public class TransferRuleServiceImpl implements TransferRuleService {
 
     @Autowired
-    TransferRuleRepository ruleRepository;
+    TransferRuleRepository repo;
 
     @Override
     public TransferRule createRule(TransferRule rule) {
-        return ruleRepository.save(rule);
+        return repo.save(rule);
     }
     @Override
     public TransferRule updateRule(Long id, TransferRule rule) {
-        if (ruleRepository.existsById(id)) {
+        if (repo.existsById(id)) {
             rule.setId(id);
-            return ruleRepository.save(rule);
+            return repo.save(rule);
         }
         throw new RuntimeException("not found");
     }
@@ -34,50 +34,17 @@ public class TransferRuleServiceImpl implements TransferRuleService {
    
     @Override
     public TransferRule getRuleById(Long id) {
-    return ruleRepository.findById(id)
+    return repo.findById(id)
             .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "TransferRule not found with id " + id)
             );
 }
     @Override
     public String DeleteData5(Long id){
-        ruleRepository.deleteById(id);
+        repo.deleteById(id);
         return "Deleted successfully";
     }
     @Override
     public List<TransferRule>getAllData5(){
-        return ruleRepository.findAll();
+        return repo.findAll();
     }
 }
-// package com.example.demo.service.impl;
-
-// import org.springframework.stereotype.Service;
-// import java.util.List;
-// import com.example.demo.entity.TransferRule;
-// import com.example.demo.repository.TransferRuleRepository;
-// import com.example.demo.service.TransferRuleService;
-
-// @Service
-// public class TransferRuleServiceImpl implements TransferRuleService {
-
-//     private final TransferRuleRepository transferRuleRepository;
-
-//     public TransferRuleServiceImpl(TransferRuleRepository transferRuleRepository) {
-//         this.transferRuleRepository = transferRuleRepository;
-//     }
-
-//     @Override
-//     public TransferRule createRule(TransferRule rule) {
-//         return transferRuleRepository.save(rule);
-//     }
-
-//     @Override
-//     public TransferRule getRuleById(Long id) {
-//         return transferRuleRepository.findById(id)
-//                 .orElseThrow(() -> new RuntimeException("Rule not found"));
-//     }
-
-//     @Override
-//     public List<TransferRule> getAllRules() {
-//         return transferRuleRepository.findAll();
-//     }
-// }
