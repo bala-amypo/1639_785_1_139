faeed01058a1fc102534a1ff094484ad909ee7c9
bala-amypo <bala@amypo.in>
2025-12-25 14:23:01 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -24,7 +24,7 @@ public class CourseContentTopic{
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="column_id",unique=true)
     private Long id;
-    @ManyToOne
+    @OnetoOne
     @JoinColumn(name="course_id",nullable=false)
     private Course course;
     @NotBlank(message="Topic name")
