057a4d6706bcc98c94bc87d225b21ba3b6567aae
bala-amypo <bala@amypo.in>
2025-12-27 08:04:46 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -4,13 +4,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.SecurityScheme;
 import java.util.List;
 
 @Configuration
 public class  SwaggerConfig {
-
+        
     @Bean
     public OpenAPI customOpenAPI() {
+         SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
